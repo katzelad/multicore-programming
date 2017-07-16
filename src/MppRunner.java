c898838216744ea39ef8ada3e7b72fed80475b0a
elad <elad@elad-U24E>
2017-07-16 22:39:44 +0300

    exercise 4

--- src/MppRunner.java
+++ src/MppRunner.java
@@ -1,45 +1,14 @@
-
-import java.util.HashMap;
-import java.util.Map;
-
-import mpp.*;
+import mpp.Ex4q7;
 
 public class MppRunner {
 
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws Exception {
 
-		final int NUM_OF_BENCHMARKS = 10;
+		for (int numThreads : new int[] { 1, 2, 4, 8, 16, 32, 64 }) {
 
-		for (int i : new int[] { 1, 2, 4, 8, 16, 32 }) {
-			String[] arg = new String[] { String.valueOf(i) };
-			System.out.println("Question 7, " + i + " threads:");
-			Map<String, Double> benchmark = new HashMap<>();
-			for (int j = 0; j < NUM_OF_BENCHMARKS; j++) {
-				Ex3q7.main(arg);
-				for (String queue : Ex3q7.benchmark.keySet()) {
-					if (benchmark.containsKey(queue))
-						benchmark.put(queue, benchmark.get(queue) + Ex3q7.benchmark.get(queue));
-					else
-						benchmark.put(queue, Ex3q7.benchmark.get(queue));
-				}
-			}
-			for (String queue : benchmark.keySet())
-				System.out.printf("%s: average of %.1f operations per second\n", queue,
-						benchmark.get(queue) / NUM_OF_BENCHMARKS);
-			System.out.println("Question 8, " + i + " threads:");
-			benchmark.clear();
-			for (int j = 0; j < NUM_OF_BENCHMARKS; j++) {
-				Ex3q8.main(arg);
-				for (String lock : Ex3q8.benchmark.keySet()) {
-					if (benchmark.containsKey(lock))
-						benchmark.put(lock, benchmark.get(lock) + Ex3q8.benchmark.get(lock));
-					else
-						benchmark.put(lock, Ex3q8.benchmark.get(lock));
-				}
-			}
-			for (String lock : benchmark.keySet())
-				System.out.printf("%s: average of %.1f ms per million iterations\n", lock,
-						benchmark.get(lock) / NUM_OF_BENCHMARKS);
+			System.out.print(numThreads + " threads: ");
+			Ex4q7.main(new String[] { Integer.toString(numThreads), "/tmp/random-graph.txt" });
+
 		}
 
 	}
--- src/mpp/Ex4q7.java
+++ src/mpp/Ex4q7.java
@@ -2,10 +2,13 @@ package mpp;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Ex4q7 {
-	
+
 	private static class Edge {
 
 		private final int weight;
@@ -20,7 +23,12 @@ public class Ex4q7 {
 
 	private static class Node {
 
+		private final int id;
 		private final List<Edge> edges = new ArrayList<>();
+
+		private Node(int id) {
+			this.id = id;
+		}
 
 		private void addEdge(Edge edge) {
 			edges.add(edge);
@@ -29,12 +37,17 @@ public class Ex4q7 {
 	}
 
 	public static void main(String[] args) throws Exception {
-		
+
 		final String[] lines = Files.readAllLines(Paths.get(args[1])).toArray(new String[0]);
 		final int numThreads = Integer.parseInt(args[0]);
 		final String[] header = lines[0].split(" ");
 		final int numNodes = Integer.parseInt(header[2]), numEdges = Integer.parseInt(header[3]);
-		final List<Node>[] nodes = new List<Node>[numNodes];
+		final List<List<Node>> table = new ArrayList<>(numNodes);
+		final List<Lock> locks = new ArrayList<>(numNodes);
+		for (int i = 0; i < numNodes; i++) {
+			table.add(new ArrayList<>());
+			locks.add(new ReentrantLock());
+		}
 
 		class GraphBuilderThread extends Thread {
 
@@ -50,9 +63,25 @@ public class Ex4q7 {
 				for (int i = from; i < to; i++) {
 					String[] line = lines[i].split(" ");
 					int src = Integer.parseInt(line[1]), dst = Integer.parseInt(line[2]),
-							weight = Integer.parseInt(line[3]), hc = Integer.hashCode(src);
-					
-					new Edge(weight, dst);
+							weight = Integer.parseInt(line[3]), hc = Integer.hashCode(src) % numNodes;
+					Lock lock = locks.get(hc);
+					lock.lock();
+					try {
+						List<Node> bucket = table.get(hc);
+						Node srcNode = null;
+						for (Node node : bucket)
+							if (node.id == src) {
+								srcNode = node;
+								break;
+							}
+						if (srcNode == null) {
+							srcNode = new Node(src);
+							bucket.add(srcNode);
+						}
+						srcNode.addEdge(new Edge(weight, dst));
+					} finally {
+						lock.unlock();
+					}
 				}
 			}
 
@@ -60,13 +89,16 @@ public class Ex4q7 {
 
 		Thread[] threads = new Thread[numThreads];
 		for (int i = 0; i < numThreads; i++)
-			threads[i] = new Thread(numEdges * i / numThreads + 1, numEdges * (i + 1) / numThreads + 1) {
-				@Override
-				public void run() {
-					for (int j = 0; j < 1000000; j++)
-						counter.getAndIncrement();
-				}
-			};
+			threads[i] = new GraphBuilderThread(numEdges * i / numThreads + 1, numEdges * (i + 1) / numThreads + 1);
+		
+		long startTime = System.currentTimeMillis();
+		for (int i = 0; i < numThreads; i++)
+			threads[i].start();
+		for (int i = 0; i < numThreads; i++)
+			threads[i].join();
+		long endTime = System.currentTimeMillis();
+		
+		System.out.println("Elapsed time: " + (endTime - startTime) + "ms");
 
 	}
 
